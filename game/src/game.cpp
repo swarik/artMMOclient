acc1d878477d1f1df1fb4a96eb92b032a58aee91
Den <xx26@bk.ru>
2024-08-27 15:53:11 +0300

    Update game.cpp

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <unistd.h>
+#include <unistd.h> // unix timer ( <windows.h> для Win32 (Sleep(5)) || <dos.h> для Win32 (delay(5)) )   //
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 
@@ -282,7 +282,7 @@ CURLcode GGet(){
 //***************
 CURLcode GFight(){
 
-	// Функция сбора ресурсов.
+	// Функция сражения.
 	CURL *hnd = curl_easy_init();
 
 	curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
